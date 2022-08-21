d7f54d9a8466cffae7571afb8241442393719810
Dima <pushokf@gmail.com>
2022-08-21 20:16:34 +0300

    made toggleButton

--- stop-watch/step-04/stop-watch.js
+++ stop-watch/step-04/stop-watch.js
@@ -1,4 +1,7 @@
 const stopWatchView = document.querySelector('#stop-watch-view');
+const toggleButton = document.getElementById('toggle-stop-watch');
+
+toggleButton.addEventListener('click', toggleStopWatch);
 
 let hundredth = 0;
 let seconds = 0;
@@ -7,16 +10,16 @@ let minutes = 0;
 function displayTime() {
     // seconds double digits
     let secondsString = seconds;
-    if(seconds < 10) {
+    if (seconds < 10) {
         secondsString = '0' + seconds;
     }
 
     // minutes double digits
     let minuteString = minutes
-    if(minutes < 99) {
+    if (minutes < 99) {
         minuteString = '0' + minutes;
     }
-    
+
     stopWatchView.innerHTML = `${minuteString}:${secondsString}:${hundredth}`;
 }
 
@@ -33,10 +36,20 @@ function handleTimeChange() {
     displayTime();
 };
 
-function startStopWatch() {
-    setInterval(handleTimeChange, 10);
+let intervalId = null;
+
+function toggleStopWatch() {
+    if (intervalId == null) {
+        // start the stopwatch
+        intervalId = setInterval(handleTimeChange, 10);
+        toggleButton.innerHTML = 'Stop';
+    } else {
+        // stop the stopwatch
+        clearInterval(intervalId);
+        intervalId = null;
+        toggleButton.innerHTML = 'Start';
+    }
 }
 
-const startButton = document.getElementById('toggle-stop-watch');
 
-startButton.addEventListener('click', startStopWatch);
+
