8c9357be04ebf1f2f367cec706a64876cafaac0a
Dima <pushokf@gmail.com>
2022-08-21 19:49:28 +0300

    added buttons

---
+++ stop-watch/step-04/stop-watch.js
@@ -0,0 +1,39 @@
+const stopWatchView = document.querySelector('#stop-watch-view');
+
+let hundredth = 0;
+let seconds = 0;
+let minutes = 0;
+
+function displayTime() {
+    // seconds double digits
+    let secondsString = seconds;
+    if(seconds < 10) {
+        secondsString = '0' + seconds;
+    }
+
+    // minutes double digits
+    let minuteString = minutes
+    if(minutes < 99) {
+        minuteString = '0' + minutes;
+    }
+    
+    stopWatchView.innerHTML = `${minuteString}:${secondsString}:${hundredth}`;
+}
+
+function handleTimeChange() {
+    hundredth++;
+    if (hundredth > 99) {
+        hundredth = 0;
+        seconds++;
+        if (seconds > 59) {
+            seconds = 0;
+            minutes++;
+        }
+    }
+    displayTime();
+};
+
+function startStopWatch() {
+    setInterval(handleTimeChange, 10);
+}
+
