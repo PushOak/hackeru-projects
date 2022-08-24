d9653b21e57b062330a9a871475f463a557168ee
Dima <pushokf@gmail.com>
2022-08-24 20:17:08 +0300

    stuff

--- timers/step-04/timers.js
+++ timers/step-04/timers.js
@@ -2,6 +2,11 @@
 function Timer(initialTime) {
     this.initialTime = initialTime;
 
+        this.createTimer = function() {
+            const timersContainer = document.querySelector('#timers-container');
+            console.log('timersContainer' , timersContainer);
+            timersContainer.innerHTML += '<div>new timer</div>';
+        }
     this.createTimer = function () {
         console.log('create timer');
     }
