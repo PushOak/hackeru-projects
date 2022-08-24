c4e2514b42cf5ee113adc706c86c31bffbb763b9
Dima <pushokf@gmail.com>
2022-08-24 19:59:02 +0300

    constructor timer

--- timers/step-04/app.js
+++ timers/step-04/app.js
@@ -1,17 +1,11 @@
 import Timer from './timers.js';
 
-// console.log(Timer);
-
-// const t1 = new Timer(50);
-// console.log('timer 1', t1);
-
-// const t2 = new Timer(40);
-// console.log('timer 2', t2);
-
 document.querySelector('#create-timer').addEventListener('click', addTimer);
 const timerInput = document.querySelector('#time-input')
 
 function addTimer() {
     const initialTime = timerInput.value;
+    const timer = new Timer(initialTime);
     console.log('value from input', initialTime);
+    console.log('timer', timer);
 }
