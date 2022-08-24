0bb94f5b92e41b8443b1e1cbfd8d81e64b560484
Dima <pushokf@gmail.com>
2022-08-24 18:55:25 +0300

    added comments

--- timers/step-03/timers.js
+++ timers/step-03/timers.js
@@ -1,3 +1,5 @@
+
+// constructor function used as a Template of creating objects
 function fullName(name, lastName) {
     this.name = name;
     this.last_name = lastName;
@@ -7,10 +9,12 @@ function fullName(name, lastName) {
     }
 }
 
-
+// create new object with fullName constructor function
 const obj1 = new fullName('Noam', 'Bob');
+//create another object
 const obj2 = new fullName('Ziggy', 'Zagga');
 
+// calling the fname() function of the object
 obj1.fname();
 obj2.fname();
 
