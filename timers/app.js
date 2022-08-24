d3759cdc771b6da0be26868621bd1d7257521c49
Dima <pushokf@gmail.com>
2022-08-24 18:03:51 +0300

    added step-02

--- timers/app.js
+++
@@ -1,5 +0,0 @@
-import object, { name, number } from './timers.js';
-
-console.log('name: ',name);
-console.log('object: ',object);
-console.log('number: ',number);
---
+++ timers/step-02/app.js
@@ -0,0 +1,5 @@
+import object, { name, number } from './timers.js';
+
+console.log('name: ', name);
+console.log('object: ', object);
+console.log('number: ', number);
