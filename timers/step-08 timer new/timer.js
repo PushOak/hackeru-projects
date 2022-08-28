9ecfba4a13e37a0bc7e5c2f469e0ad87144dd085
Dima <pushokf@gmail.com>
2022-08-28 19:02:50 +0300

    created delete button

--- timers/step-08 timer new/timer.js
+++ timers/step-08 timer new/timer.js
@@ -1,14 +1,30 @@
 // timer.js
 export default function Timer(initialTime) {
     let time = initialTime;
+    let timerElement;
+
+    function createStartButton() {
+        const startButton = document.createElement('button');
+        startButton.innerText = 'Start';
+        timerElement.appendChild(startButton);
+    }
+
+    function createDeleteButton() {
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        timerElement.appendChild(deleteButton);
+    }
 
     this.createTimer = function () {
         console.log('create timer', time);
 
-        const timerElement = document.createElement('div');
+        timerElement = document.createElement('div');
         timerElement.innerText = time;
+
+        createStartButton();
+        createDeleteButton();
 
         document.querySelector('#timers-container').appendChild(timerElement);
     };
-};
+}
 
